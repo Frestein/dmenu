a2854b311b27aaf928977b3dbc6cf3c64366858b
Frestein <Frestein@tuta.io>
2024-06-15 19:04:19 +0500

    fix: change min width

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int draw_input = 1;                  /* -noi option; if 0, the input will not be drawn by default */
 static int opacity = 0;                     /* -o  option; if 0, then alpha is disabled */
 static int center = 1;                      /* -c  option; if 0, dmenu won't be centered on the screen */
-static int min_width = 600;                 /* minimum width when centered */
+static int min_width = 500;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char *fonts[] =
 {
